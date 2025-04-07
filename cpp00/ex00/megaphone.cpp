88f70634f9fe75503957288dad4ed54aa803b632
Natália Leite Schardosim <131466597+schardot@users.noreply.github.com>
2025-04-07 15:13:56 +0200

    megaphone

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cctype>
+
+int main(int argc, char **argv) {
+    if (argc == 1) {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+        return (0);
+    }
+    for (int i = 1; i < argc; i++) {
+        for (int j = 0; argv[i][j] != '\0'; j++) {
+            argv[i][j] = std::toupper(argv[i][j]);
+            std::cout << argv[i][j];
+        }
+        std::cout<< ' ';
+    }
+    std::cout << std::endl;
+}
