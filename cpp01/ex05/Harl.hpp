2d248a728cfe174b7cc38783974a6255fbee8bd0
Natália Leite Schardosim <131466597+schardot@users.noreply.github.com>
2025-04-23 11:38:21 +0200

    Harl

---
+++ cpp01/ex05/Harl.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class Harl
+{
+private:
+    void debug(void);
+    void info(void);
+    void warning(void);
+    void error(void);
+
+public:
+    Harl();
+    ~Harl();
+    void complain(std::string level);
+};
---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,7 @@
+#include "Harl.hpp"
+
+int main(void) {
+
+    Harl h;
+    h.complain("DEBUG");
+}
