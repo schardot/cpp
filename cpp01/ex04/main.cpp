00a593a28268a7b8e67f879c1a9c64af0b771a98
Natália Leite Schardosim <131466597+schardot@users.noreply.github.com>
2025-04-23 10:40:40 +0200

    Replace

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+std::string replaceIt(std::string line, std::string find, std::string change) {
+    size_t pos = 0;
+    while (pos != std::string::npos)
+    {
+        pos = line.find(find);
+        if (pos != std::string::npos)
+        {
+            line.erase(pos, find.length());
+            line.insert(pos, change);
+        }
+    }
+    return (line);
+}
+
+int main(int ac, char **av)
+{
+    if (ac != 4)
+    {
+        std::cout << "wrong ac: " << ac << std::endl;
+        return (1);
+    }
+    std::string repName = av[1];
+    repName.append(".replace");
+
+    std::fstream myFile;
+    std::ofstream replaceFile;
+
+    myFile.open(av[1]);
+    replaceFile.open(repName);
+
+    std::string line;
+    if (myFile.is_open())
+    {
+        while (std::getline(myFile, line))
+        {
+            line = replaceIt(line, av[2], av[3]);
+            replaceFile << line << std::endl;
+        }
+    }
+
+    myFile.close();
+    replaceFile.close();
+    return (0);
+}
