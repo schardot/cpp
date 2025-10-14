31f2fc0db23c02ba2047b70172b894c63ea305e7
Natália Leite Schardosim <131466597+schardot@users.noreply.github.com>
2025-10-14 12:38:10 +0200

    refactor: unify test output formatting by using printTestLine function

--- cpp05/ex03/test/InternTest.cpp
+++ cpp05/ex03/test/InternTest.cpp
@@ -2,23 +2,31 @@
 #include "../include/Colors.hpp"
 #include "../include/Intern.hpp"
 
+static void printTestLine(std::string num, std::string message)
+{
+    std::cout << std::endl
+              << RESET << BOLDYELLOW << "\n===== TEST "
+              << num << ": "
+              << message
+              << " ===== " << RESET << std::endl;
+}
+
 int main()
 {
-
-    std::cout << RESET << BOLDYELLOW << "\n===== TEST 1: INTERN CREATION FORM CONSTRUCTION & DESTRUCTION =====" << RESET << std::endl;
+    printTestLine("1", "FUNCTIONALITY –  INTERN CREATION FORM CONSTRUCTION & DESTRUCTION");
     {
         std::cout << std::endl;
         Intern int1;
     }
 
-    std::cout << RESET << BOLDYELLOW << "\n===== TEST 2: INTERN CREATION FORM COPY CONSTRUCTOR =====" << RESET << std::endl;
+    printTestLine("2", "FUNCTIONALITY –  INTERN FORM COPY CONSTRUCTOR");
     {
         std::cout << std::endl;
         Intern int2;
         Intern int2copy(int2);
     }
 
-    std::cout << RESET << BOLDYELLOW << "\n===== TEST 3: ERROR: FAIL TO CREATE A FORM DUE TO TYPO =====" << RESET << std::endl;
+    printTestLine("3", "ERROR – FAIL TO CREATE A FORM DUE TO TYPO");
     {
         std::cout << std::endl;
         Intern int3;
@@ -26,7 +34,7 @@ int main()
         std::cout << std::endl;
     }
 
-    std::cout << RESET << BOLDYELLOW << "\n===== TEST 4: FUNCTIONALITY: SUCCESFULLY CREATE ALL FORMS =====" << RESET << std::endl;
+    printTestLine("4", "FUNCTIONALITY – SUCCESFULLY CREATE ALL FORMS");
     {
         std::cout << std::endl;
         Intern int3;
@@ -40,37 +48,42 @@ int main()
         std::cout << std::endl;
     }
 
-    std::cout << RESET << BOLDYELLOW << "\n===== TEST 5: INTERN CREATE SHRUBBERY FORM, BUREAUCRAT SIGNS AND EXECUTE =====" << RESET << std::endl;
+    printTestLine("5", "FUNCTIONALITY – INTERN CREATE ALL FORMS, BUREAUCRAT SIGNS AND EXECUTE");
     {
-        std::cout << std::endl;
+        try
+        {
+            std::cout << std::endl;
 
-        Bureaucrat bur1("Herr Bureaucrat", 1);
-        std::cout << std::endl;
+            Bureaucrat bur1("Herr Bureaucrat", 1);
+            std::cout << std::endl;
 
-        Intern int3;
-        std::cout << std::endl;
+            Intern int3;
+            std::cout << std::endl;
 
-        AForm *f1 = int3.makeForm("shrubbery creation", "target");
-        AForm *f2 = int3.makeForm("robotomy request", "target");
-        AForm *f3 = int3.makeForm("presidential pardon", "target");
-        std::cout << std::endl;
+            AForm *f1 = int3.makeForm("shrubbery creation", "target");
+            AForm *f2 = int3.makeForm("robotomy request", "target");
+            AForm *f3 = int3.makeForm("presidential pardon", "target");
+            std::cout << std::endl;
 
-        bur1.signForm(*f1);
-        bur1.executeForm(*f1);
-        delete f1;
-        std::cout << std::endl;
+            bur1.signForm(*f1);
+            bur1.executeForm(*f1);
+            delete f1;
+            std::cout << std::endl;
 
-        bur1.signForm(*f2);
-        bur1.executeForm(*f2);
-        delete f2;
-        std::cout << std::endl;
+            bur1.signForm(*f2);
+            bur1.executeForm(*f2);
+            delete f2;
+            std::cout << std::endl;
 
-        bur1.signForm(*f3);
-        bur1.executeForm(*f3);
-        delete f3;
-        std::cout << std::endl;
-
-        std::cout << std::endl;
+            bur1.signForm(*f3);
+            bur1.executeForm(*f3);
+            delete f3;
+            std::cout << std::endl;
+        }
+        catch (std::exception &e)
+        {
+            std::cout << BOLDRED << "Caught exception during test 5: " << e.what() << RESET << std::endl;
+        }
     }
 
     return 0;
--- cpp05/ex03/test/PresidentialTest.cpp
+++ cpp05/ex03/test/PresidentialTest.cpp
@@ -2,34 +2,42 @@
 #include "../include/Colors.hpp"
 #include "../include/PresidentialPardonForm.hpp"
 
+static void printTestLine(std::string num, std::string message)
+{
+    std::cout << std::endl
+              << RESET << BOLDYELLOW << "\n===== TEST "
+              << num << ": "
+              << message
+              << " ===== " << RESET << std::endl;
+}
+
 int main()
 {
-
-    std::cout << RESET << BOLDYELLOW << "\n===== TEST 1: PRESIDENTIAL PARDON FORM CONSTRUCTION & DESTRUCTION =====" << RESET << std::endl;
+    printTestLine("1", "FUNCTIONALITY –  PRESIDENTIAL PARDON CREATION FORM CONSTRUCTION & DESTRUCTION");
     {
         std::cout << std::endl;
         PresidentialPardonForm form1("oi");
     }
 
-    std::cout << RESET << BOLDYELLOW << "\n===== TEST 2: PRESIDENTIAL PARDON FORM COPY CONSTRUCTOR =====" << RESET << std::endl;
+    printTestLine("2", "FUNCTIONALITY –  PRESIDENTIAL PARDON FORM COPY CONSTRUCTOR");
     {
         std::cout << std::endl;
         PresidentialPardonForm form2("oi");
         PresidentialPardonForm form2copy(form2);
     }
 
-
-    std::cout << RESET << BOLDYELLOW << "\n===== TEST 3: FUNCTIONALITY: SUCESSFULLY SIGN A FORM =====" << RESET << std::endl;
+    printTestLine("3", "FUNCTIONALITY –  SUCESSFULLY SIGN A FORM");
     {
         std::cout << GRAY << "Quick info: In order to sign Presidential Form, a Bureaucrat needs grade 25." << RESET << std::endl;
         std::cout << std::endl;
         PresidentialPardonForm form6("oi");
-        Bureaucrat b1("Bureaucrat 1", 5);
+        Bureaucrat b1("Bureaucrat 1", 25);
         b1.signForm(form6);
+        // b1.executeForm(form6); //Bureaucrat won't be able to execute
         std::cout << std::endl;
     }
 
-    std::cout << RESET << BOLDYELLOW << "\n===== TEST 4: FUNCTIONALITY: SUCESSFULLY EXECUTE PARDON =====" << RESET << std::endl;
+    printTestLine("4", "FUNCTIONALITY –  SUCESSFULLY EXECUTE FORM");
     {
         std::cout << GRAY << "Quick info: In order to execute Presidential Form, a Bureaucrat needs grade 5." << RESET << std::endl;
         PresidentialPardonForm form6("oi");
@@ -39,13 +47,23 @@ int main()
         std::cout << std::endl;
     }
 
-    std::cout << RESET << BOLDYELLOW << "\n===== TEST 5: ERROR: TRYING TO EXECUTE ROBOTOMY WITH NOT ENOUGH GRADE =====" << RESET << std::endl;
+    printTestLine("5", "ERROR –  TRYING TO EXECUTE FORM WITH NOT ENOUGH GRADE");
     {
         std::cout << GRAY << "Quick info: In order to execute Presidential Form, a Bureaucrat needs grade 5." << RESET << std::endl;
         std::cout << std::endl;
         PresidentialPardonForm form6("oi");
         Bureaucrat b1("Bureaucrat 3", 6);
         b1.signForm(form6);
+        b1.executeForm(form6);
+        std::cout << std::endl;
+    }
+
+    printTestLine("6", "ERROR –  TRYING TO EXECUTE UNSIGNED FORM");
+    {
+        std::cout << std::endl;
+        PresidentialPardonForm form6("oi");
+        Bureaucrat b1("Bureaucrat 6", 3);
+        // b1.signForm(form6);
         b1.executeForm(form6);
         std::cout << std::endl;
     }
