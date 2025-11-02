a0ba0a77bdcd91c9923e4deed3b4a934f9df14a8
Natália Leite Schardosim <131466597+schardot@users.noreply.github.com>
2025-11-02 19:51:56 +0100

    feat: ex02

---
+++ cpp08/ex02/src/MutantStack.cpp
@@ -0,0 +1,7 @@
+#include "MutantStack.hpp"
+
+template <typename T>
+MutantStack<T>::MutantStack() : std::stack<T>() {}
+
+template <typename T>
+MutantStack<T>::MutantStack(const MutantStack<T> &obj) : std::stack<T>(obj) {}
